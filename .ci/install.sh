cd7c1b69dbe52a1310e6115bc97d5c668c4fbdd3
Benjamin Kampmann <ben@gnunicorn.org>
2017-02-28 10:54:03 +0100

    setting tag

--- .ci/install.sh
+++ .ci/install.sh
@@ -14,11 +14,12 @@ main() {
     fi
 
     # This fetches latest stable release
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
-                       | cut -d/ -f3 \
-                       | grep -E '^v[0-9.]+$' \
-                       | $sort --version-sort \
-                       | tail -n1)
+    local tag='allow-prefix'
+    # $(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+    #                    | cut -d/ -f3 \
+    #                    | grep -E '^v[0-9.]+$' \
+    #                    | $sort --version-sort \
+    #                    | tail -n1)
     echo cross version: $tag
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
