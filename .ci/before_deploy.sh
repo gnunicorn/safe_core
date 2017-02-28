1c5f3d04460cc068d33916b5df076fae3244d055
Benjamin Kampmann <ben@gnunicorn.org>
2017-02-28 15:22:39 +0100

    better build with all

--- .ci/before_deploy.sh
+++ .ci/before_deploy.sh
@@ -3,7 +3,14 @@
 
 set -ex
 
-main() {
+build() {
+    test -f Cargo.lock || cargo generate-lockfile
+
+    cargo clean
+    cross rustc --all --target $TARGET --release --features="$FEATURE"
+}
+
+package() {
     local src=$(pwd) \
           stage=
 
@@ -15,11 +22,6 @@ main() {
             stage=$(mktemp -d -t tmp)
             ;;
     esac
-
-    test -f Cargo.lock || cargo generate-lockfile
-
-    cargo clean
-    cross rustc --path $CRATE_NAME --target $TARGET --release --features="$FEATURE"
 
     # copy linux
     cp target/$TARGET/release/lib$CRATE_NAME* $stage/
@@ -36,8 +38,6 @@ main() {
     fi
     cd $src
 
-
-
     rm -rf $stage
 }
 
@@ -45,37 +45,34 @@ main() {
 declare -a CRATES=("safe_app" "safe_authenticator")
 declare -a FEATURES=("use-mock-routing testing,dev")
 
-for crate in "${CRATES[@]}"
+if [ $TRAVIS_OS_NAME = linux ]; then
+
+    declare -a TARGETS=("i686-unknown-linux-gnu,linux-x32"
+                        "x86_64-unknown-linux-gnu,linux-x64"
+                        "i686-unknown-linux-musl"
+                        "x86_64-unknown-linux-musl"
+                        )
+else
+    declare -a TARGETS=("x86_64-apple-darwin,darwin-x64"
+                        "i686-apple-darwin,darwin-x32"
+                        )
+fi
+
+for target in "${TARGETS[@]}"
 do
-    export CRATE_NAME="$crate"
-    if [ $TRAVIS_OS_NAME = linux ]; then
+    export TARGET=${target%,*}              # before comma
+    export TARGET_NAME=${target#*,}         # after comma
 
-        declare -a TARGETS=("i686-unknown-linux-gnu,linux-x32"
-                            "x86_64-unknown-linux-gnu,linux-x64"
-                            "i686-unknown-linux-musl"
-                            "x86_64-unknown-linux-musl"
-                            )
-    else
-        declare -a TARGETS=("x86_64-apple-darwin,darwin-x64"
-                            "i686-apple-darwin,darwin-x32"
-                            )
-    fi
-
-    for target in "${TARGETS[@]}"
+    for feat in "${FEATURES[@]}"
     do
-        export TARGET=${target%,*}       # before comma
-        export TARGET_NAME=${target#*,}   # after comma
+        export FEATURE=${feat%,*}           # before comma
+        export FEATURE_NAME="-${feat#*,}"   # after comma
+        build
 
-        for feat in "${FEATURES[@]}"
+        for crate in "${CRATES[@]}"
         do
-            export FEATURE=${feat%,*}          # before comma
-            export FEATURE_NAME="-${feat#*,}"   # after comma
-            main
+            export CRATE_NAME="$crate"
+            package                         # package each crate
         done
     done
-
-    # # move the package up
-    # cp *.zip ../
-    # # and leave the create
-    # cd ..
 done
