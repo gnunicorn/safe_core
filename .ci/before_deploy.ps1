0b8b8b93758613acf25fb82b96fb4aeaea733736
Benjamin Kampmann <ben@gnunicorn.org>
2017-03-06 17:29:19 +0100

    First cross platform prerelease of 0.1.0

--- .ci/before_deploy.ps1
+++ .ci/before_deploy.ps1
@@ -4,14 +4,18 @@
 $SRC_DIR = $PWD.Path
 $STAGE = [System.Guid]::NewGuid().ToString()
 
+Set-Location $CRATE_NAME
+# build the specific item
+cargo rustc --target $($Env:TARGET) --features "$($Env:FEATURES)" --release --lib
+
 Set-Location $ENV:Temp
 New-Item -Type Directory -Name $STAGE
 Set-Location $STAGE
 
-$ZIP = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET_NAME).zip"
+$ZIP = "$SRC_DIR\$CRATE_NAME$($Env:RELEASE_SUFFIX)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET_NAME).zip"
 
 # TODO Update this to package the right artifacts
-Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\system_uri.dll" '.\'
+Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\$CRATE_NAME.dll" '.\'
 Copy-Item "$SRC_DIR\README.md" '.\'
 Copy-Item "$SRC_DIR\LICENSE" '.\'
 
