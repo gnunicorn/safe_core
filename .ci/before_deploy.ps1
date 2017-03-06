c673480bbc4d9a552abed317996ffbd7b9207587
Benjamin Kampmann <ben@gnunicorn.org>
2017-03-06 17:29:19 +0100

    fix/CI move win build intp Appveyor

--- .ci/before_deploy.ps1
+++ .ci/before_deploy.ps1
@@ -4,14 +4,18 @@
 $SRC_DIR = $PWD.Path
 $STAGE = [System.Guid]::NewGuid().ToString()
 
+Set-Location $args[0]
+# build the specific item
+cargo rustc --target %TARGET% --features "%FEATURES" --release --lib
+
 Set-Location $ENV:Temp
 New-Item -Type Directory -Name $STAGE
 Set-Location $STAGE
 
-$ZIP = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET_NAME).zip"
+$ZIP = "$SRC_DIR\$($args[0])$($Env:RELEASE_SUFFIX)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET_NAME).zip"
 
 # TODO Update this to package the right artifacts
-Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\system_uri.dll" '.\'
+Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\$($args[0]).dll" '.\'
 Copy-Item "$SRC_DIR\README.md" '.\'
 Copy-Item "$SRC_DIR\LICENSE" '.\'
 
